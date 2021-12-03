692c3d89a335f4e5a33da9e007ffc83c89c85a37
Thomas Bruce <44057980+the-Bruce@users.noreply.github.com>
2021-12-03 23:39:41 +0000

    Day 3 Part 1

--- src/Days/Day03.hs
+++ src/Days/Day03.hs
@@ -13,26 +13,55 @@ import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
+import qualified Data.Attoparsec.Text as AP
 import Data.Void
+import Data.Functor (($>))
+import Data.Attoparsec.ByteString.Char8 (isDigit)
+import Data.Text (unpack)
 {- ORMOLU_ENABLE -}
+
+type Binary = [Int]
+binToDec :: Binary -> Int
+binToDec = binToDec' 0
+  where
+    binToDec' a (b:xs) = binToDec' (a*2 + b) xs
+    binToDec' a [] = a
+
+decToBin :: Int -> Binary
+decToBin i = i `div` 2 : decToBin (i `mod` 2)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = sepBy binary endOfLine
+  where
+    binary = do
+      a <- AP.takeWhile isDigit
+      pure (map asInt (unpack a))
+    asInt :: Char -> Int
+    asInt '0' = 0
+    asInt '1' = 1
+    asInt _ = error "Invalid Char"
+
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Binary]
 
-type OutputA = Void
+type OutputA = Int
 
 type OutputB = Void
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA inp = binToDec (map epsilon val) * binToDec (map gamma val)
+  where
+    len = length inp
+    val = map sum $ transpose inp
+    epsilon x = if x>(len `div` 2) then 1 else 0
+    gamma x = if x<(len `div` 2) then 1 else 0
+    
 
 ------------ PART B ------------
 partB :: Input -> OutputB
