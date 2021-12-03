74a45b6acf872146f483a6d20adc8ff10dbacc32
Thomas Bruce <44057980+the-Bruce@users.noreply.github.com>
2021-12-03 22:57:23 +0000

    Day 2

--- src/Days/Day02.hs
+++ src/Days/Day02.hs
@@ -14,26 +14,51 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
 import Data.Void
+import Data.Functor (($>))
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
+data SubAction = Forward Int | Up Int | Down Int deriving (Show, Eq)
+
+type Location = (Int, Int)
+type Pose = (Location, Int)
+
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = sepBy operation endOfLine
+  where
+    operation = do
+      act <- choice [string "forward" $> Forward, string "up" $> Up, string "down" $> Down]
+      skipSpace
+      act <$> decimal
+
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [SubAction]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA inp = locSum $ foldl' updateState (0,0) inp
+  where
+    locSum (x, z) = x*z
+    updateState :: Location -> SubAction -> Location
+    updateState (x, z) (Forward i) = (x+i, z)
+    updateState (x, z) (Up i) = (x, z-i)
+    updateState (x, z) (Down i) = (x, z+i)
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB inp = poseSum $ foldl' updateState ((0,0),0) inp
+  where
+    poseSum (loc, _) = locSum loc
+    locSum (x, z) = x*z
+    updateState :: Pose -> SubAction -> Pose
+    updateState ((x, z), aim) (Forward i) = ((x+i, z+(aim*i)), aim)
+    updateState (loc, aim) (Up i) = (loc, aim-i)
+    updateState (loc, aim) (Down i) = (loc, aim+i)
