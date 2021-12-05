7617f29017a0a357e99ff62febfe731531b50899
Thomas Bruce <44057980+the-Bruce@users.noreply.github.com>
2021-12-05 17:34:09 +0000

    Day 5 (warning, very slow...)

--- src/Days/Day05.hs
+++ src/Days/Day05.hs
@@ -19,21 +19,63 @@ import Data.Void
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
+type Point = (Int, Int)
+
+type Line = (Point, Point)
+
+type Space = [(Point, Int)]
+
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = line `sepBy` skipSpace
+  where
+    line = do
+      x1 <- decimal
+      _ <- string ","
+      y1 <- decimal
+      _ <- string " -> "
+      x2 <- decimal
+      _ <- string ","
+      y2 <- decimal
+      pure ((x1, y1), (x2, y2))
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Line]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
+
+isOnLine :: Line -> Point -> Bool
+isOnLine ((x1, y1), (x2, y2)) (x, y)
+  | x1 == x2 = x == x1 && y <= max y1 y2 && y >= min y1 y2
+  | y1 == y2 = y == y1 && x <= max x1 x2 && x >= min x1 x2
+  | otherwise = sx * (x - x1) == sy * (y - y1) && y <= max y1 y2 && y >= min y1 y2 && x <= max x1 x2 && x >= min x1 x2
+  where
+    dx = x1 - x2
+    dy = y1 - y2
+    sx = signum dx
+    sy = signum dy
+
+initialSpace :: Space
+initialSpace = [((x, y), 0) | y <- [0 .. 1000], x <- [0 .. 1000]]
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA inp = length $ filter ((>= 2) . snd) $ foldr markSpace initialSpace (filter orthog inp)
+  where
+    orthog :: Line -> Bool
+    orthog ((x1, y1), (x2, y2)) = x1 == x2 || y1 == y2
+    markSpace :: Line -> Space -> Space
+    markSpace l s = map (updatePoint l) s
+    updatePoint :: Line -> (Point, Int) -> (Point, Int)
+    updatePoint l (p, b) = (p, b + (if isOnLine l p then 1 else 0))
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB inp = length $ filter ((>= 2) . snd) $ foldr markSpace initialSpace inp
+  where
+    markSpace :: Line -> Space -> Space
+    markSpace l s = map (updatePoint l) s
+    updatePoint :: Line -> (Point, Int) -> (Point, Int)
+    updatePoint l (p, b) = (p, b + (if isOnLine l p then 1 else 0))
