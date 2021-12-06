88b96bc8e759c359c1ce87bec4b102eb8a664a51
Thomas Bruce <44057980+the-Bruce@users.noreply.github.com>
2021-12-06 15:00:07 +0000

    Day 6

--- src/Days/Day06.hs
+++ src/Days/Day06.hs
@@ -14,6 +14,7 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
 import Data.Void
+import Data.Bifunctor (bimap, first)
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +22,41 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = decimal `sepBy` string ","
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Int]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA lst = length $ periodPasses 80 lst
+  where
+    day 0 = [6,8]
+    day n = [n-1]
+    dayPasses (x:xs) = day x ++ dayPasses xs
+    dayPasses [] = []
+    periodPasses n = foldr1 (.) $ replicate n dayPasses
 
 ------------ PART B ------------
+groupBy2 :: (a -> a -> Bool) -> [a] -> [[a]]
+groupBy2 = go [] where
+  go acc comp [] = acc
+  go acc comp (h:t) =
+    let (hs, nohs) = partition (comp h) t
+    in go ((h:hs):acc) comp nohs
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB lst = sum $ map generation $ fix 256 $ groupBy2 (==) lst
+        where
+          generation :: (Int, Int) -> Int
+          generation (0, fish) = 2*fish
+          generation (days, fish) | days < 0 = fish
+          generation (days, fish) = generation (days-7, fish) + generation (days-9, fish)
+          fix :: Int -> [[Int]] -> [(Int, Int)]
+          fix days fishes = map (first ((days-1) -)) fish
+            where
+              fish = map (\x -> (head x, length x)) fishes
